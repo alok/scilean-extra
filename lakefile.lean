af75301d1fbc36982f65589bc66c889e32083288
Alok Singh <alokbeniwal@gmail.com>
2024-05-31 01:00:22 -0700

    build deps

--- lakefile.lean
+++ lakefile.lean
@@ -1,9 +1,16 @@
 import Lake
 open Lake DSL
 require scilean from git "https://github.com/lecopivo/SciLean" @ "master"
+-- require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.2.0"
+require LSpec from git "https://github.com/lurk-lab/LSpec.git" @ "main"
 
-package «scilean-extra» where
+package ScileanExtra where
   -- add package configuration options here
+  moreLinkArgs := #[
+    "-L./.lake/packages/LeanCopilot/.lake/build/lib",
+    "-lctranslate2"
+  ]
+
 
 lean_lib «ScileanExtra» where
   -- add library configuration options here
